8dd619d4b7adc5fbfd6afb918d85e28054391cc7
Caiofran <caio.ricciardi@soulasalle.com.br>
2021-11-30 21:06:46 -0300

    update

---
+++ Av2Poo/src/jdbc/ConnectionFactory.java
@@ -0,0 +1,44 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	
+	private static final String DRIVE = "com.mysql.jdbc.Driver";
+	private static final String url = "jdbc:mysql://database-2.cuve3svqyxqb.us-east-1.rds.amazonaws.com:3306/bancoprog?useTimezone=true&serverTimezone=UTC"; 
+	private static final String user = "admin"; 
+	private static final String pass = "password"; 
+	
+	public static Connection getConnection() {
+        try {
+        	return DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro na conexão com o Banco de Dados!",e);
+        }
+	}
+
+	public static void closeConnection(Connection con) {
+		if(con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void closeConnection(Connection con, PreparedStatement stmt) {
+		closeConnection(con);
+		
+		if(stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
