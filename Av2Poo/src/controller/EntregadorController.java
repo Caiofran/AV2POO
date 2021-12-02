1c01dbe35df7a10d88aebaf923806bb103f7a634
Caiofran <caio.ricciardi@soulasalle.com.br>
2021-12-02 15:11:50 -0300

    update

--- Av2Poo/src/controller/EntregadorController.java
+++ Av2Poo/src/controller/EntregadorController.java
@@ -7,27 +7,27 @@ import bo.Entregador;
 import model.EntregadorDAO;
 
 public class EntregadorController {
-    public void create(Entregador habilidade) {
+    public void create(Entregador entregador) {
         try {
             EntregadorDAO model = new EntregadorDAO();
-            model.create(habilidade);
+            model.create(entregador);
         } catch(Exception e) {
-			System.out.println("<Controller> Erro ao tentar inserir Habilidade: " + e);
+			System.out.println("<Controller> Erro ao tentar inserir Entregador: " + e);
 		}
 	}
 
 	public List<Entregador> read() {
-		List<Entregador> listaDeMissao = new ArrayList<Entregador>();
+		List<Entregador> listaDeEntregador = new ArrayList<Entregador>();
 		
 		try {
 			EntregadorDAO model = new EntregadorDAO();
-            listaDeMissao = model.read();
+            listaDeEntregador = model.read();
             		
 		} catch(Exception e) {
-			System.out.println("<Controller> Erro ao tentar ler Habilidade: " + e);
+			System.out.println("<Controller> Erro ao tentar ler Entregador: " + e);
 		}
 
-		return listaDeMissao;
+		return listaDeEntregador;
 	}
 
 	public void delete(Entregador missao) {
@@ -36,7 +36,7 @@ public class EntregadorController {
             model.delete(missao);
             		
 		} catch(Exception e) {
-			System.out.println("<Controller> Erro ao esconder Habilidade: " + e);
+			System.out.println("<Controller> Erro ao esconder Entregador: " + e);
 		}
 	}
 	
@@ -46,7 +46,7 @@ public class EntregadorController {
             model.update(missao);
             		
 		} catch(Exception e) {
-			System.out.println("<Controller> Erro ao tentar atualizar Habilidade: " + e);
+			System.out.println("<Controller> Erro ao tentar atualizar Entregador: " + e);
 		}
 	}
 }
